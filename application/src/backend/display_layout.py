205fd540fa512e82bee5d0210d241ce5460d7551
Emma <100003456+emmanmoore@users.noreply.github.com>
2024-11-10 13:24:36 -0500

    Add files via upload

---
+++ application/src/backend/display_layout.py
@@ -0,0 +1,15 @@
+#this defines the layout to display list view by default
+current_layout = 'list_view'
+
+#this function checks the active layout and displays it
+def display_layout():
+    global current_layout
+    if current_layout == 'list_view':
+        #insert command to display list view layout (may need connected to Flask)
+        print('list_view is being displayed')
+    elif current_layout == 'calendar_view':
+        #insert command to display calendar view layout (may need connected to Flask)
+        print('calendar_view is being displayed')
+    else:
+        print('There was an issue displaying the layout. Try again.')
+    
